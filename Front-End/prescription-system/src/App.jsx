c28a78be80e7df98bcc6f3ce86e56027894a7ef9
Thaseenthan <thaseenthan0812@gmail.com>
2026-03-10 17:43:37 +0530

    feat: initialize prescription system with React and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created App component with a title
    - Added Tailwind CSS import in index.css
    - Set up main entry point for React application
    - Configured Vite with React and Tailwind CSS plugins

---
+++ Front-End/prescription-system/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+
+        <h1 className="text-blue-700">Prescription System</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
